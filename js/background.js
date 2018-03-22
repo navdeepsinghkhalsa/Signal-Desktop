19519dee5572a6c189cf6a7a1c0fc84fbc2dd3ac
Daniel Gasienica <daniel@signal.org>
2018-03-22 11:56:33 -0400

    Improve user messaging during initialization
    - We first show a localized loading message.
    - If initialization takes longer than a certain threshold, we show a different
      ‘optimization’ message.
    - If initialization is below the threshold the message change is canceled right
      before the regular loading screen.

--- js/background.js
+++ js/background.js
@@ -16,6 +16,7 @@
 
     const { Errors, Message } = window.Signal.Types;
     const { context: migrationsContext } = window.Signal.Migrations;
+    const { Views } = window.Signal;
 
     // Implicitly used in `indexeddb-backbonejs-adapter`:
     // https://github.com/signalapp/Signal-Desktop/blob/4033a9f8137e62ed286170ed5d4941982b1d3a64/components/indexeddb-backbonejs-adapter/backbone-indexeddb.js#L569
@@ -73,6 +74,8 @@
         return accountManager;
     };
 
+    const cancelInitializationMessage = Views.Initialization.setMessage();
+    console.log('Start IndexedDB migrations');
     storage.fetch();
 
     // We need this 'first' check because we don't want to start the app up any other time
@@ -137,6 +140,7 @@
             connect(true);
         });
 
+        cancelInitializationMessage();
         var appView = window.owsDesktopApp.appView = new Whisper.AppView({el: $('body')});
 
         Whisper.WallClockListener.init(Whisper.events);
